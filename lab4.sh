eeed58b1a3a0f1a16b4a69030bc1e247b0104b9b
Jakub51727 <dsw51727@student.dsw.edu.pl>
2024-05-09 17:14:39 +0200

    koniec czesci 2

--- lab4.sh
+++ lab4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
 	echo "Dzis jest $(date)"
 	exit 0
 fi
-if [ "$1" == "--logs" ]; then
+if [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
 
 	for ((i=1; i<100; i++)); do
 		echo "plik: log$i.txt" > "log$i.txt"
@@ -21,13 +21,19 @@ if [ "$1" == "--logs 30" ]; then
 	done
 	exit 0
 fi
-if [ "$1" == "--help" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h"]; then
 	echo "opcje:"
-	echo "--date : wyswietla dzisiejsza date"
-	echo "--logs : tworzy 100 plikow z nazwa, data i skryptem pierwotym"
+	echo "--date(-d) : wyswietla dzisiejsza date"
+	echo "--logs(-l) : tworzy 100 plikow z nazwa, data i skryptem pierwotym"
 	echo "--logs 30 : tworzy 30 plikow jak powyzszy skrypt"
-	echo "--help : dostepne opcje"
+	echo "--help(h) : dostepne opcje"
+	exit 0
+fi
+if [ "$1" == "--init" ] || [ "$1" == "-i" ] ; then
+	git clone https://github.com/Jakub51727/cwiczenia.git
+	export PATH=$PATH:$(pwd)/cwiczenia
 	exit 0
 fi
 echo "nie istnieje podana flaga. Wpisz '--help'"
 exit 1
+
