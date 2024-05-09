424793f7c2546842b89760848383e93c71de37d7
Jakub51727 <dsw51727@student.dsw.edu.pl>
2024-05-09 17:16:30 +0200

    koniec

--- lab4.sh
+++ lab4.sh
@@ -27,12 +27,25 @@ if [ "$1" == "--help" ] || [ "$1" == "-h"]; then
 	echo "--logs(-l) : tworzy 100 plikow z nazwa, data i skryptem pierwotym"
 	echo "--logs 30 : tworzy 30 plikow jak powyzszy skrypt"
 	echo "--help(h) : dostepne opcje"
+	echo "init(-i) klonuje repo do folderu w ktorym zostal uruchomiony skrypt oraz ustawia PATH"
+	echo "--error(-e) [liczba] tworzy errorx/errorx.txt w ilosci rownej podanej liczbie"
 	exit 0
 fi
 if [ "$1" == "--init" ] || [ "$1" == "-i" ] ; then
 	git clone https://github.com/Jakub51727/cwiczenia.git
 	export PATH=$PATH:$(pwd)/cwiczenia
 	exit 0
+fi
+if [ "$1" == "--error" ] || [ "$1" == "-e" ]; then
+    if [ -z "$2" ]; then
+        num_errors=100
+    else
+        num_errors=$2
+    fi
+    for ((i=1; i<=$num_errors; i++)); do
+        create_error_file "$i"
+    done
+    exit 0
 fi
 echo "nie istnieje podana flaga. Wpisz '--help'"
 exit 1
